775096e4081e1993d5de047cab98bc8ec2cdc417
Andres Melara <andres.melara.t@gmail.com>
2023-05-06 04:25:58 -0400

    Implement react router and Link tag

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,13 @@
+import { Link } from "react-router-dom";
 import NavBar from "./nav-bar";
 
 export default function Header({ setCurrentPage }) {
   return (
     <header className="items-center justify-between sm:flex sm:flex-row sm:pl-8">
       <h1 className="py-4 text-2xl font-bold">
-        <a
-          href="/"
-          onClick={(e) => {
-            e.preventDefault();
-            setCurrentPage("/");
-          }}
-        >
+        <Link to="/" className="text-blue-500">
           Router Demo
-        </a>
+        </Link>
       </h1>
       <NavBar setCurrentPage={setCurrentPage} />
     </header>
