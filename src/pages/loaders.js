6b0f41306ad84b9e8e90b7fa2d1519ad61519d4e
Andres Melara <andres.melara.t@gmail.com>
2023-05-11 20:05:52 -0400

    Set up api

---
+++ src/pages/loaders.js
@@ -0,0 +1,2 @@
+import { defer } from "react-router-dom";
+import apiService from "../services/api";
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import ky from "ky";
+export default {
+  indexThoughts() {
+    return ky.get("http://localhost:3001/thoughts").json();
+  },
+};
