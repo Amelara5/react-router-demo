1ecc6852c07ab2abd56601c7fe8bce866f225d33
Andres Melara <andres.melara.t@gmail.com>
2023-05-11 20:06:46 -0400

    Re-factor router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import Layout from "./components/layout";
 import { About, Contact, LoginRegister } from "./pages";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { path: "/about", element: <About /> },
-      { path: "/contact", element: <Contact /> },
-      { path: "/login", element: <LoginRegister /> },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "/about", element: <About /> },
+//       { path: "/contact", element: <Contact /> },
+//       { path: "/login", element: <LoginRegister /> },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<LoginRegister />} />
+    </Route>
+  )
+);
 
 function App() {
   return (
