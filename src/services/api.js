82bd0667b748e40343d0acb32addc64f077ea92a
Andres Melara <andres.melara.t@gmail.com>
2023-05-11 23:44:09 -0400

    Update api to retrieve thoughts from one author

--- src/services/api.js
+++ src/services/api.js
@@ -3,4 +3,17 @@ export default {
   indexThoughts() {
     return ky.get("http://localhost:3001/thoughts").json();
   },
+
+  async showThoughts(author) {
+    const allThoughts = await this.indexThoughts();
+    const thoughts4Author = allThoughts.filter(
+      (thought) => thought.author === author
+    );
+
+    if (!thoughts4Author.length) {
+      throw new Error("No thoughts found");
+    }
+
+    return thoughts4Author;
+  },
 };
