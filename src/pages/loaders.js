1c9005b7db19a3786c073ff395faaf18c92d0eee
Andres Melara <andres.melara.t@gmail.com>
2023-05-11 20:38:02 -0400

    Add thoughts and use defer in it

--- src/pages/loaders.js
+++ src/pages/loaders.js
@@ -1,2 +1,8 @@
 import { defer } from "react-router-dom";
 import apiService from "../services/api";
+
+export const loadThoughts = () => {
+  const thoughts = apiService.indexThoughts();
+
+  return defer({ thoughts });
+};
