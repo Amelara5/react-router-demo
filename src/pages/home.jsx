49c9161e122166feef9649428c283967952e6731
Andres Melara <andres.melara.t@gmail.com>
2023-05-11 21:38:44 -0400

    Add handleing errors

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import { Await, useLoaderData } from "react-router-dom";
+import Error from "../components/error";
 import Loading from "../components/loading";
 import ThoughtList from "../components/thoughts/thought-list";
 
@@ -7,7 +8,7 @@ export default function Home() {
   const { thoughts } = useLoaderData();
   return (
     <Suspense fallback={<Loading />}>
-      <Await resolve={thoughts}>
+      <Await resolve={thoughts} errorElement={<Error />}>
         <ThoughtList />
       </Await>
     </Suspense>
