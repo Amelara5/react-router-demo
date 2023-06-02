a5721036974215ba247d453952c24df7fa2a2eab
Andres Melara <andres.melara.t@gmail.com>
2023-06-01 21:06:19 -0400

    Customize cypress for ESLint and Vite

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "cypress";
+import vitePreprocessor from "cypress-vite";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       // implement node event listeners here
+      on("file:preprocessor", vitePreprocessor());
     },
+    viteConfig,
   },
 });
