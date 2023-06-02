2e40c07f48776fde7d20beb63736ecbfd6717683
Andres Melara <andres.melara.t@gmail.com>
2023-06-02 19:54:01 -0400

    test: :white_check_mark: Assure the desire user is clicked
    It should only show the thoughts of the user that was 'clicked'

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -11,4 +11,20 @@ describe("Home 🏠", () => {
     cy.visit("http://localhost:5173/");
     cy.get("[data-cy=thought-list] li").should("have.length", 4);
   });
+
+  it("shows just thoughts for the clicked author", () => {
+    cy.intercept("/thoughts", {
+      fixture: "thoughts.json",
+    });
+    cy.visit("http://localhost:5173/");
+
+    // 'user2click' will be last in the list
+    // cy.get("ul li:last").click();
+    cy.contains("user2click").click();
+
+    // Just one thought
+    cy.get("[data-cy=thought-list] li").should("have.length", 1);
+
+    // The thought is from 'user2click'
+  });
 });
