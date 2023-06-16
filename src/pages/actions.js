3fc7c6e43820e629c5a5379ed78e6cdb9724313b
Andres Melara <andres.melara.t@gmail.com>
2023-06-15 20:55:07 -0400

    Implement try-catch to handle errors

--- src/pages/actions.js
+++ src/pages/actions.js
@@ -1,9 +1,28 @@
+import { redirect } from "react-router-dom";
+import api from "../services/api";
+import { validateRegistrationPasswords } from "../services/utils";
+
 export const registerOrLogin = async ({ request }) => {
-  console.log("Submitted");
-  console.log(request);
+  //   console.log("Submitted");
+  //   console.log(request);
 
   const fd = await request.formData();
   const submittedUser = Object.fromEntries(fd);
-  console.log(submittedUser);
-  return null;
+
+  //   console.log(submittedUser);
+
+  try {
+    validateRegistrationPasswords(
+      submittedUser.password,
+      submittedUser.confirmedPassword
+    );
+
+    const { token } = await api.registerUser(submittedUser);
+    setTokenCookie(token);
+
+    return redirect("/");
+  } catch (error) {
+    return error.message;
+  }
+  return token;
 };
