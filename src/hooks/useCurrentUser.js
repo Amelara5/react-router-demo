943c129c39952652989ecfe66dc140998ba5074a
Andres Melara <andres.melara.t@gmail.com>
2023-07-10 21:16:37 -0400

    delete comment

--- src/hooks/useCurrentUser.js
+++ src/hooks/useCurrentUser.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { getTokenCookie, decodeUserFromTokenCookie } from "../services/utils";
+import { decodeUserFromTokenCookie } from "../services/utils";
 
 export default function useCurrentUser() {
   const user = decodeUserFromTokenCookie();
-  //   const user = getTokenCookie(); <---- Ask why not this?
   const [currentUser, setCurrentUser] = useState(user);
 
   return [currentUser, setCurrentUser];
