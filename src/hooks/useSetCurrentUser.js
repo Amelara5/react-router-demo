6d4c147a2b02077c56e54e18b6411985eb36a18c
Andres Melara <andres.melara.t@gmail.com>
2023-07-11 20:03:21 -0400

    add import for 'decodeUserFromTokenCookie'

--- src/hooks/useSetCurrentUser.js
+++ src/hooks/useSetCurrentUser.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
+import { decodeUserFromTokenCookie } from "../services/utils";
 
 export function useSetCurrentUser() {
   const [useCurrentUser, setCurrentUser] = useOutletContext();
 
   useEffect(() => {
-    setCurrentUser(decodeUserFromTokenCookie);
+    setCurrentUser(decodeUserFromTokenCookie());
   });
   return useCurrentUser;
 }
